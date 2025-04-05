e6decd27c0544d2e65c25199afc3aac66e1ac426
unknown <abu040810@gmail.com>
2025-04-05 14:33:24 +0500

    ozgaruvchilar

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let a=5;
+// 
+
+// ozgaruvchilar->xotiradan malum bir joyni saqlash uchun ishlatiladi 
+// console.log -> natija va xatolikni chiqaradi
+
+// c tili int,float,char 
+// 4 xil bular avtomatik ,ikkinchisi var,3-si let,4-si const
+// avtomatik
+a=5;  // mashnaqa numberlarga 
 console.log(a);
+
+// var
+
+// ozgaruvchini qiymati ozgarsa
+var b=10; // var ism='toshmat'
+var b=15;// var ism='muhammad ibn vahhob'
+console.log(b);
+
+// var ham kechik ozgaruvchini qiymatini o'zgartirish mumkin    
+
+
+// let ham ozgaruvchini bir turi
+let c=20;
+c=25;
+console.log(c);
+
+// u let degan ozgaruvchini nomini olsagina ishlaydi
+
+// const esa ozgarmas
+const d=30;
+d=35; // bu xato
+console.log(d);
